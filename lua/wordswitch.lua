4c1d21541a5eca35e5407cbeefaff3235b201308
tandy <tandy1229@qq.com>
2022-11-29 17:53:38 +0800

    format: use stylua to format the code

--- lua/wordswitch.lua
+++ lua/wordswitch.lua
@@ -3,48 +3,46 @@ local g, fn = vim.g, vim.fn
 function M.wordswitch()
 	local dictionary, sub
 	if g.custom_wordswitch_definitions then
-		dictionary =  g.custom_wordswitch_definitions .. g.wordswitch_definitions
+		dictionary = g.custom_wordswitch_definitions .. g.wordswitch_definitions
 	else
 		dictionary = g.wordswitch_definitions
 	end
 
-	local word = fn.expand('<cword>')
+	local word = fn.expand("<cword>")
 
 	for _, v in ipairs(dictionary) do
-        local temp = 0
-        for _, d in ipairs(v) do
-            temp = temp + 1
-            if word == d then
-                if #v == temp then
-                    sub = v[1]
-                    break
-                else
-                    sub = v[temp + 1]
-                    break
-                end
-            end
-        end
+		local temp = 0
+		for _, d in ipairs(v) do
+			temp = temp + 1
+			if word == d then
+				if #v == temp then
+					sub = v[1]
+					break
+				else
+					sub = v[temp + 1]
+					break
+				end
+			end
+		end
 	end
 	if sub then
-		vim.cmd('normal! ciw' .. sub)
+		vim.cmd("normal! ciw" .. sub)
 	end
 end
 return M
 
-
-
 -- another solve
-    -- for _, v in ipairs(dictionary) do
-    --     local idx = fn.index(v, word)
-    --     
-    --     if idx > -1 then
-    --         if idx == #v - 1 then
-    --             sub = v[1]
-    --         else
-    --             sub = v[idx + 2]
-    --         end
-    --
-    --         vim.cmd('normal! ciw' .. sub)
-    --         break
-    --     end
-    -- end
+-- for _, v in ipairs(dictionary) do
+--     local idx = fn.index(v, word)
+--
+--     if idx > -1 then
+--         if idx == #v - 1 then
+--             sub = v[1]
+--         else
+--             sub = v[idx + 2]
+--         end
+--
+--         vim.cmd('normal! ciw' .. sub)
+--         break
+--     end
+-- end
