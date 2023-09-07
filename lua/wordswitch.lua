b770e64f0a4c6a2b5d1484a13e80a3eea1ff3a50
tandy <tandy1229@qq.com>
2023-09-07 15:48:22 +0800

    feat(core): awesome update to support the lower & upper case in changes:-)
    Now you can also change word with the upper and the lower case!!!
    
    for example:
    
    now it has a word 'True'
    and now you can use the plugin to change it to 'False'
    you don't need to add { 'True‘, 'False' } to your dictorary.
    
    so as 'TRUE' to 'FALSE'
    
    It's a great changes.

--- lua/wordswitch.lua
+++ lua/wordswitch.lua
@@ -1,6 +1,11 @@
 local M = {}
 local g, fn = vim.g, vim.fn
 
+-- TODO: change it to a lua type plugin
+-- but now it use vim's type plugin:-)
+-- such as vim.g instead of _G.
+-- In the future times I will find some
+-- plugin to learn the way it become.
 function M.wordswitch()
 	local dictionary, sub
 	if g.custom_wordswitch_definitions then
@@ -8,40 +13,37 @@ function M.wordswitch()
 	else
 		dictionary = g.wordswitch_definitions
 	end
-
+	---@type string word
 	local word = fn.expand("<cword>")
+	local word_first_char = word:sub(1, 1)
+	local word_low_case = word:lower()
+	local found = false
 	-- TODO: find a way which can search words more effective
 	-- without using vim.fn.index() api
 	for _, v in ipairs(dictionary) do
+		if found then
+			break
+		end
 		local temp = 0
 		for _, d in ipairs(v) do
 			temp = temp + 1
-			if word == d then
+			if word_low_case == d then
 				sub = v[temp % #v + 1]
-				goto dest
+				found = true
+				break
 			end
 		end
 	end
-	::dest::
 	if sub then
+		if word == word:upper() then
+			sub = sub:upper()
+		elseif word_first_char == word_first_char:upper() then
+			sub = sub:gsub("^%l", string.upper)
+		else
+			sub = sub:lower()
+		end
 		vim.cmd("normal! ciw" .. sub)
 	end
 end
 
 return M
-
--- another solve
--- for _, v in ipairs(dictionary) do
---     local idx = fn.index(v, word)
---
---     if idx > -1 then
---         if idx == #v - 1 then
---             sub = v[1]
---         else
---             sub = v[idx + 2]
---         end
---
---         vim.cmd('normal! ciw' .. sub)
---         break
---     end
--- end
